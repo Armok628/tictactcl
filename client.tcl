37c2847f7e7600aaa91f6590ab91d3f985a38992
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-08-07 17:58:36 -0500

    Client improvements

--- client.tcl
+++ client.tcl
@@ -5,15 +5,17 @@ update
 toplevel .client
 wm title .client "Connection"
 wm resizable .client 0 0
-wm title .client "Tic-Tac-Tcl Client"
 proc start {} {
-	client_start [.client.entry get]
+	client_start $::address
 	destroy .client
 }
 ttk::button .client.button -text "Connect" -command start
-ttk::entry .client.entry
+ttk::entry .client.entry -textvariable address
+set address "Address"
 bind .client.entry <Return> start
+bind .client.entry <KP_Enter> start
 grid .client.entry -column 0 -row 0
 grid .client.button -column 0 -row 1
 grab set .client
 focus .client.entry
+.client.entry selection range 0 end
