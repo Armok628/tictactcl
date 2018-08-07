4a85fbac14420cbae0581b2b90be9320e47a39af
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-08-07 16:26:38 -0500

    Improved client

--- client.tcl
+++ client.tcl
@@ -1,13 +1,16 @@
 #!/usr/bin/tclsh
-package require Tk
+source tictac.tcl
 update
 toplevel .client
-ttk::entry .client.entry
-ttk::button .client.button -command {
-	source tictac.tcl
+wm resizable .client 0 0
+wm title .client "Tic-Tac-Tcl Client"
+proc start {} {
 	client_start [.client.entry get]
 	destroy .client
 }
+ttk::button .client.button -text "Connect" -command start
+ttk::entry .client.entry
+bind .client.entry <Return> start
 grid .client.entry -column 0 -row 0
 grid .client.button -column 0 -row 1
 grab set .client
