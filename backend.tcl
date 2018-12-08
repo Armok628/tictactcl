994e1a55e21ef4d0f8e92e5f8d1f38dfe22c4e55
Armok628 <Mark.Johnson.62899@gmail.com>
2018-12-08 17:38:17 -0600

    Reorganize backend

--- backend.tcl
+++ backend.tcl
@@ -1,19 +1,16 @@
 #!/usr/bin/tclsh
-proc new_nested_board {} {
-	lrepeat 9 [lrepeat 9 "_"]
+proc new_game {} {
+	list x -1 [lrepeat 9 [lrepeat 9 "_"]]
 }
+proc turn {game} {lindex $game 0}
+proc next_board {game} {lindex $game 1}
+proc board {game} {lindex $game 2}
 proc next_turn {turn} {
 	switch $turn {
 	x {return o}
 	o {return x}
 	default {error "Invalid turn"}
 	}
-}
-proc turn {game} {lindex $game 0}
-proc next_board {game} {lindex $game 1}
-proc board {game} {lindex $game 2}
-proc new_game {} {
-	list x -1 [new_nested_board]
 }
 global win_methods
 set win_methods [list \
