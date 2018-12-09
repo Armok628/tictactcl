f27f6a78d7142def6f4ab57f19d7501e9b002b59
Armok628 <Mark.Johnson.62899@gmail.com>
2018-12-09 00:40:36 -0600

    Update demo

--- demo.tcl
+++ demo.tcl
@@ -1,9 +1,10 @@
 #!/usr/bin/tclsh
 source tictac.tcl
 handle_move [random_move $game_state]
+lassign $argv x o
 while {[check_game_over $game_state] eq "_"} {
-	catch {handle_move [think $game_state 5]}
-	catch {handle_move [think $game_state 25]}
+	catch {handle_move [think $game_state $o]}
+	catch {handle_move [think $game_state $x]}
 }
 puts "Winner: [check_game_over $game_state]"
-if {[llength $argv]} {after 2500; exit}
+if {[llength $argv]>2} {after 2500; exit}
