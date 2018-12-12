747aeab3a45cf4684224fc9219ca859e751c7643
Armok628 <Mark.Johnson.62899@tamu.edu>
2018-12-11 22:56:52 -0600

    Improved demo

--- demo.tcl
+++ demo.tcl
@@ -1,12 +1,27 @@
 #!/usr/bin/tclsh
 source tictac.tcl
-source smtai.tcl
 update
-handle_move [random_move $game_state]
+set smt 0
+set quit 0
 lassign $argv x o
-while {[set winner [check_game_over $game_state]] eq "_"} {
+foreach arg [lrange $argv 2 end] {
+	switch $arg {
+		-smt {set smt 1}
+		-quit {set quit 1}
+	}
+}
+if {$smt} {
+	source smtai.tcl
+}
+handle_move [random_move $game_state]
+while {1} {
+	if {[set winner [check_game_over $game_state]] ne "_"} break
 	handle_move [think $game_state $o]
+	if {[set winner [check_game_over $game_state]] ne "_"} break
 	handle_move [think $game_state $x]
 }
 puts "Winner: $winner"
-if {[llength $argv]>2} {after 2500; exit}
+if {$quit} {
+	after 2500
+	exit
+}
